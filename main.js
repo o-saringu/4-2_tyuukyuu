691ddcc8bbc6de2e332b3cca198077e10798ac3c
o-saringu <147477572+o-saringu@users.noreply.github.com>
2023-12-07 17:37:36 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function buttonClick(){
+	one.value = one.value.replace(/\s+/g, "");
+	two.value = two.value.replace(/\s+/g, "");
+	
+	if (one.value == "(intx=30;x<width;x+=30)"){
+		text1.innerHTML = "正解";
+	}else{
+		text1.innerHTML = "不正解　解答：(int x=30; x<width; x+=30)";
+	}
+
+	if (two.value == "(inty=30;y<height;y+=30)"){
+		text2.innerHTML = "正解";
+	}else{
+		text2.innerHTML = "不正解　解答：(int y=30; y<height; y+=30)";
+	}
+
+	
+}
+
+//解答欄１
+var textBox1 = document.getElementById("one");
+const text1 = document.getElementById("text1");
+
+//解答欄２
+var textBox2 = document.getElementById("two");
+const text2 = document.getElementById("text2");
+
+let checkButton = document.getElementById("btn");
+checkButton.addEventListener("click", buttonClick);
+
